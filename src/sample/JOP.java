f8b97301fdc44419df569635bbb0dbc498ba717d
David Ulloa <dulloa6310@gmail.com>
2021-01-18 17:49:41 -0500

    Added ability to play default mazes.

--- src/sample/JOP.java
+++ src/sample/JOP.java
@@ -6,4 +6,8 @@ public class JOP {
     public static void msg(String message) {
         JOptionPane.showMessageDialog(null, message);
     }
+
+    public static String input(String message) {
+        return JOptionPane.showInputDialog(message);
+    }
 }
