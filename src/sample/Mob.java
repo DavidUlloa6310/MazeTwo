f2431134a498eaebb46f23ab2dff1069e19a2b42
David Ulloa <dulloa6310@gmail.com>
2021-01-20 20:20:51 -0500

    Just changed an incomplete line of code.

--- src/sample/Mob.java
+++ src/sample/Mob.java
@@ -94,8 +94,6 @@ public class Mob extends Character {
                     getMaze().end();
                 }
             }
-
-            if (this.getCoordX() == )
         }
         return false;
     }
